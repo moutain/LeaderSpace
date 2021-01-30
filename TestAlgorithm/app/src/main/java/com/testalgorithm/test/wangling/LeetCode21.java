6a4b95a1472374ac6db08a3e2fe5df9a600085e9
wl <328375181@qq.com>
2021-01-30 14:50:15 +0800

    LeetCode21

---
+++ TestAlgorithm/app/src/main/java/com/testalgorithm/test/wangling/LeetCode21.java
@@ -0,0 +1,98 @@
+package com.testalgorithm.test.wangling;
+
+import java.util.List;
+
+class LeetCode21 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode();
+        listNode.val = 1;
+        listNode.next = new ListNode();
+        listNode.next.val = 2;
+        listNode.next.next = new ListNode();
+        listNode.next.next.val = 4;
+
+        ListNode listNode1 = new ListNode();
+        listNode1.val = 1;
+        listNode1.next = new ListNode();
+        listNode1.next.val = 3;
+        listNode1.next.next = new ListNode();
+        listNode1.next.next.val = 4;
+        mergeTwoLists(listNode, listNode1);
+
+    }
+
+    /**
+     * 迭代思想解法 不需要新建ListNode 直接在原有链表上操作
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (null == l1){
+            return l2;
+        }else if (null == l2){
+            return l1;
+        }else if (l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+
+    }
+
+
+    /**
+     * 迭代思想解法 新建链表 比较后将数据存入新链表
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+        ListNode listNode = new ListNode(-1);
+        ListNode temp = listNode;
+        while (l1 != null && l2 != null){
+            if (l1.val <= l2.val){
+                temp.next = l1;
+                l1 = l1.next;
+            }else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 == null? l2: l1;
+        return listNode.next;
+    }
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
