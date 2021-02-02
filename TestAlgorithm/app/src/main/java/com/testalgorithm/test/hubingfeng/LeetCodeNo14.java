915154b0e655f2e9a5de9fe0ab177a3d9b3f5303
hahaheihei1 <github7895123>
2021-02-02 16:43:43 +0800

    上传代码

---
+++ TestAlgorithm/app/src/main/java/com/testalgorithm/test/hubingfeng/LeetCodeNo14.java
@@ -0,0 +1,35 @@
+package com.testalgorithm.test.hubingfeng;
+
+public class LeetCodeNo14 {
+
+    //编写一个函数来查找字符串数组中的最长公共前缀。
+    //
+    //如果不存在公共前缀，返回空字符串 ""。
+
+    public static void main(String[] args) {
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public static String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
